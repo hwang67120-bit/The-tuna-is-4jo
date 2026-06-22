cd9c2fb36cf40978ac1e70507a5fb6e0064ba4d3
HwangSunNam <hwang67120@gmail.com>
2026-06-22 19:09:41 +0900

    Initial commit

---
+++ src/main/java/com/example/thetunais4joteamproject/TheTunaIs4joTeamProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.thetunais4joteamproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheTunaIs4joTeamProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheTunaIs4joTeamProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/thetunais4joteamproject/TheTunaIs4joTeamProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thetunais4joteamproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheTunaIs4joTeamProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
