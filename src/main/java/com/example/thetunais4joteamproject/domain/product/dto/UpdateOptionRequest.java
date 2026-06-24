08f6111cdd5e55bd76437f09e741c7ce7815eaed
베려하는마음 <102654500+sighingpotato@users.noreply.github.com>
2026-06-24 16:30:26 +0900

    fix: Add validation annotations to UpdateOptionRequest fields
    Added validation annotations for fields in UpdateOptionRequest.

--- src/main/java/com/example/thetunais4joteamproject/domain/product/dto/UpdateOptionRequest.java
+++ src/main/java/com/example/thetunais4joteamproject/domain/product/dto/UpdateOptionRequest.java
@@ -8,10 +8,18 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class UpdateOptionRequest {
-	private Long optionId;
-	private int optionStock;
-	private int additionalPrice;
-	private OptionStatus status;
+	
+	@NotNull(message = "옵션 ID는 필수 입력 값입니다.")
+    private Long optionId;
+
+    @Min(value = 0, message = "옵션 재고는 0개 이상이어야 합니다.")
+    private int optionStock;
+
+    @Min(value = 0, message = "추가 금액은 0원 이상이어야 합니다.")
+    private int additionalPrice;
+
+    @NotNull(message = "옵션 상태값은 필수입니다.")
+    private OptionStatus status;
 
 	private UpdateOptionRequest(Long optionId, int optionStock, int additionalPrice, OptionStatus status) {
         this.optionId = optionId;
