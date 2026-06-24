64c4aff40defa71c0c9e0676b22225dc7ae9024f
hwang67120-bit <hwang67120@gmail.com>
2026-06-24 12:25:11 +0900

    feat: 맵버 리포지토리

---
+++ src/main/java/com/example/thetunais4joteamproject/domain/user/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package com.example.thetunais4joteamproject.domain.user.repository;
+
+import com.example.thetunais4joteamproject.domain.user.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    boolean existsByEmail(String email);
+}
