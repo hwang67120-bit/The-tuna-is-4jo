eafaa6b1c1c9dc7f81dadbb95dfc1ba8265decc7
devdong1231 <79241089+devdong1231@users.noreply.github.com>
2026-06-24 14:47:21 +0900

    feat: 결제 생성 메서드 추가
    - todo 부분은 추후 변경 예정

---
+++ src/main/java/com/example/thetunais4joteamproject/domain/payment/service/PaymentService.java
@@ -0,0 +1,35 @@
+package com.example.thetunais4joteamproject.domain.payment.service;
+
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.thetunais4joteamproject.domain.payment.entity.Payment;
+import com.example.thetunais4joteamproject.domain.payment.repository.PaymentRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class PaymentService {
+
+	private final PaymentRepository paymentRepository;
+
+	// order 검증이 완료되었다는 가정 하에 작성하는 메서드 입니다.
+	@Transactional
+	public Payment createPayment(/*Order order*/){
+		// 쿠폰가를 제외한 실 결제 금액 계산, 아래 코드는 추후 제거 예정
+		// todo - pgAmount 변경
+		Integer pgAmount = 0;
+		//Integer pgAmount = order.getTotalAmount() - 쿠폰가격
+		
+		// Portone Id 생성
+		String portonePaymentId = "pay-" + UUID.randomUUID();
+
+		// todo - 주석 제거
+		Payment payment = Payment.createPendingPayment(/*order, */portonePaymentId, /*order.getTotalAmount, */ pgAmount);
+
+		return paymentRepository.save(payment);
+	}
+}
