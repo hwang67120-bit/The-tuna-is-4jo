5b1b8107183f7529a7f51db600327ff87ee6cc2a
sighingpotato <jot0202@naver.com>
2026-06-24 20:02:21 +0900

    refactor: dto 레코드 형식으로 변경

--- src/main/java/com/example/thetunais4joteamproject/domain/product/dto/RepresentStockRequest.java
+++ src/main/java/com/example/thetunais4joteamproject/domain/product/dto/RepresentStockRequest.java
@@ -1,10 +1,9 @@
 package com.example.thetunais4joteamproject.domain.product.dto;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.Min;
 
-@Getter
-@NoArgsConstructor
-public class RepresentStockRequest {
-	private int stockQuantity;
+public record RepresentStockRequest(
+        @Min(value = 0, message = "대표 재고 수량은 0개 이상이어야 합니다.")
+        int stockQuantity
+) {
 }
