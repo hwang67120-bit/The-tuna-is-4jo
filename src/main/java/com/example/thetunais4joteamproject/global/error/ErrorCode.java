5adf17288dc2a36abdf53a245d9067f83a5d2c4c
devdong1231 <79241089+devdong1231@users.noreply.github.com>
2026-06-24 14:21:41 +0900

    feat: PaymentStatus 추가

--- src/main/java/com/example/thetunais4joteamproject/global/error/ErrorCode.java
+++ src/main/java/com/example/thetunais4joteamproject/global/error/ErrorCode.java
@@ -4,12 +4,17 @@ import org.springframework.http.HttpStatus;
 
 public enum ErrorCode {
 
+
+    PAYMENT_INVALID_STATUS(HttpStatus.BAD_REQUEST, "변경할 수 없는 결제 상태입니다."),
     BAD_REQUEST(HttpStatus.BAD_REQUEST, "요청 내용을 확인해 주세요"),
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "로그인이 필요합니다"),
     NOT_FOUND(HttpStatus.NOT_FOUND, "찾을 수 없습니다"),
     CONFLICT(HttpStatus.CONFLICT, "요청을 처리할 수 없습니다"),
     SERVICE_UNAVAILABLE(HttpStatus.SERVICE_UNAVAILABLE, "현재 서비스를 이용할 수 없습니다"),
     GATEWAY_TIMEOUT(HttpStatus.GATEWAY_TIMEOUT, "응답 시간이 초과되었습니다");
+
+
+
 
     private final HttpStatus httpStatus;
     private final String message;
