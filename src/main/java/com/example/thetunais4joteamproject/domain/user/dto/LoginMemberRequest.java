51e50f30d67879e438be8f5bf1414f3f2bc2b22f
hwang67120-bit <hwang67120@gmail.com>
2026-06-24 14:54:44 +0900

    feat: 로그인 초안

---
+++ src/main/java/com/example/thetunais4joteamproject/domain/user/dto/LoginMemberRequest.java
@@ -0,0 +1,14 @@
+package com.example.thetunais4joteamproject.domain.user.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+public record LoginMemberRequest(
+        @NotBlank
+        @Pattern(regexp = "^[A-Za-z0-9]+@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)+$")
+        String email,
+
+        @NotBlank
+        String password
+) {
+}
