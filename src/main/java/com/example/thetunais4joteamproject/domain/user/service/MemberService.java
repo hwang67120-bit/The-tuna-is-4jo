8352b64093e8c0e4b62ea85814bae02f68acdd58
hwang67120-bit <hwang67120@gmail.com>
2026-06-24 14:04:33 +0900

    feat: 서비스 추가

---
+++ src/main/java/com/example/thetunais4joteamproject/domain/user/service/MemberService.java
@@ -0,0 +1,52 @@
+﻿package com.example.thetunais4joteamproject.domain.user.service;
+
+import com.example.thetunais4joteamproject.domain.user.dto.CreateMemberRequest;
+import com.example.thetunais4joteamproject.domain.user.dto.CreateMemberResponse;
+import com.example.thetunais4joteamproject.domain.user.dto.GetMemberEmailCheckResponse;
+import com.example.thetunais4joteamproject.domain.user.entity.Member;
+import com.example.thetunais4joteamproject.domain.user.repository.MemberRepository;
+import com.example.thetunais4joteamproject.global.error.BusinessException;
+import com.example.thetunais4joteamproject.global.error.ErrorCode;
+import com.example.thetunais4joteamproject.global.util.PasswordEncryptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+    private final PasswordEncryptor passwordEncryptor;
+
+    public GetMemberEmailCheckResponse getEmailAvailability(String email) {
+        boolean available = !memberRepository.existsByEmail(email);
+
+        return GetMemberEmailCheckResponse.from(available);
+    }
+
+    /** 회원가입 **/
+    @Transactional
+    public CreateMemberResponse create(CreateMemberRequest request) {
+        validateDuplicateEmail(request.email());
+
+        String encryptedPassword = passwordEncryptor.encrypt(request.password());
+        String normalizedPhoneNumber = normalizePhoneNumber(request.phoneNumber());
+        Member savedMember = memberRepository.save(
+                Member.create(request.email(), encryptedPassword, request.name(), normalizedPhoneNumber)
+        );
+
+        return CreateMemberResponse.from(savedMember);
+    }
+
+    private void validateDuplicateEmail(String email) {
+        if (memberRepository.existsByEmail(email)) {
+            throw BusinessException.from(ErrorCode.CONFLICT);
+        }
+    }
+
+    private String normalizePhoneNumber(String phoneNumber) {
+        return phoneNumber.replace("-", "");
+    }
+}
