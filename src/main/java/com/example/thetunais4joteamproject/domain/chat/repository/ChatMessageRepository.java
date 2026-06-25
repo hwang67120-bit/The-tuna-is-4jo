833ed395f56f6e4c5d6226dd98c1c19bc1a7b9d3
hwang67120-bit <hwang67120@gmail.com>
2026-06-26 01:34:50 +0900

    fix:

--- src/main/java/com/example/thetunais4joteamproject/domain/chat/repository/ChatMessageRepository.java
+++ src/main/java/com/example/thetunais4joteamproject/domain/chat/repository/ChatMessageRepository.java
@@ -1,6 +1,6 @@
 package com.example.thetunais4joteamproject.domain.chat.repository;
 
-import com.example.thetunais4joteamproject.domain.chat.ChatMessage;
+import com.example.thetunais4joteamproject.domain.chat.entity.ChatMessage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
