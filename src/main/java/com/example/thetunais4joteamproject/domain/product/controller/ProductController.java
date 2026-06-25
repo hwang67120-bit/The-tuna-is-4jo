99915f4db16a1a6b08bdb9c1ae067fc1e3bf66bb
sighingpotato <jot0202@naver.com>
2026-06-25 12:55:22 +0900

    fix: 컨트롤러 및 서비스 수정

--- src/main/java/com/example/thetunais4joteamproject/domain/product/controller/ProductController.java
+++ src/main/java/com/example/thetunais4joteamproject/domain/product/controller/ProductController.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import com.example.thetunais4joteamproject.domain.product.dto.CreateProductRequest;
