89a9b6a655da173ddf74af2fc5e3c70a8c71bec9
hwang67120-bit <hwang67120@gmail.com>
2026-06-25 09:58:24 +0900

    fix: 권한 부분 수정

--- src/main/java/com/example/thetunais4joteamproject/domain/product/controller/ProductController.java
+++ src/main/java/com/example/thetunais4joteamproject/domain/product/controller/ProductController.java
@@ -1,70 +1,71 @@
 package com.example.thetunais4joteamproject.domain.product.controller;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import com.example.thetunais4joteamproject.domain.product.dto.CreateProductRequest;
 import com.example.thetunais4joteamproject.domain.product.dto.CreateProductResponse;
 import com.example.thetunais4joteamproject.domain.product.dto.RepresentStockRequest;
 import com.example.thetunais4joteamproject.domain.product.dto.UpdateOptionRequest;
 import com.example.thetunais4joteamproject.domain.product.service.ProductService;
 import com.example.thetunais4joteamproject.global.common.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
 @RequiredArgsConstructor
 public class ProductController {
 
-    private final ProductService productService;
+	private final ProductService productService;
 
-    /**
-     * 상품 생성
-     */
-    @PostMapping
-    public ResponseEntity<ApiResponse<CreateProductResponse>> create(
-            @RequestBody
-            CreateProductRequest createProductRequest
-    ) {
-        Long productId = productService.createProduct(createProductRequest);
+	/**
+	 * 상품 생성
+	 */
+	@PostMapping
+	public ResponseEntity<ApiResponse<CreateProductResponse>> create(
+		@AuthenticationPrincipal
+		Long memberId,
+		@RequestBody
+		CreateProductRequest createProductRequest
+	) {
 
-        CreateProductResponse createProductResponse = CreateProductResponse.from(productId);
+		Long productId = productService.createProduct(memberId, createProductRequest);
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.ok(createProductResponse));
-    }
+		CreateProductResponse createProductResponse = CreateProductResponse.from(productId);
 
-    /**
-     * 상품 세부 옵션, 상태, 추가금액 일괄 변경
-     */
-    @PutMapping("/{productId}/options")
-    public ResponseEntity<ApiResponse<Void>> updateProductOptions(
-            @PathVariable
-            Long productId,
-            @RequestBody
-            List<UpdateOptionRequest> updateOptionRequests
-    ) {
-        productService.updateOptionStocks(productId, updateOptionRequests);
+		return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.ok(createProductResponse));
+	}
 
-        return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.ok(null));
-    }
+	/**
+	 * 상품 세부 옵션, 상태, 추가금액 일괄 변경
+	 */
+	@PutMapping("/{productId}/options")
+	public ResponseEntity<ApiResponse<Void>> updateProductOptions(
+		@PathVariable
+		Long productId,
+		@RequestBody
+		List<UpdateOptionRequest> updateOptionRequests
+	) {
+		productService.updateOptionStocks(productId, updateOptionRequests);
 
-    /**
-     * 상품 대표 재고 변경
-     */
-    @PutMapping("/{productId}/stock")
-    public ResponseEntity<ApiResponse<Void>> updateProductStock(
-            @PathVariable
-            Long productId,
-            @RequestBody
-            RepresentStockRequest representStockRequest
-    ) {
-        productService.updateRepresentativeStock(productId, representStockRequest);
+		return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.ok(null));
+	}
 
-        return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.ok(null));
-    }
+	/**
+	 * 상품 대표 재고 변경
+	 */
+	@PutMapping("/{productId}/stock")
+	public ResponseEntity<ApiResponse<Void>> updateProductStock(
+		@PathVariable
+		Long productId,
+		@RequestBody
+		RepresentStockRequest representStockRequest
+	) {
+		productService.updateRepresentativeStock(productId, representStockRequest);
+
+		return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.ok(null));
+	}
 }
