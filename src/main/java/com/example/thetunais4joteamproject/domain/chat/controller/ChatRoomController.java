6362a1d6678b637339dc09eac33d81d5e2780453
hwang67120-bit <hwang67120@gmail.com>
2026-06-26 11:00:36 +0900

    feat: 채팅방 참여, 목록조회, 조회

--- src/main/java/com/example/thetunais4joteamproject/domain/chat/controller/ChatRoomController.java
+++ src/main/java/com/example/thetunais4joteamproject/domain/chat/controller/ChatRoomController.java
@@ -2,6 +2,9 @@ package com.example.thetunais4joteamproject.domain.chat.controller;
 
 import com.example.thetunais4joteamproject.domain.chat.dto.CreateChatRoomRequest;
 import com.example.thetunais4joteamproject.domain.chat.dto.CreateChatRoomResponse;
+import com.example.thetunais4joteamproject.domain.chat.dto.GetChatRoomListResponse;
+import com.example.thetunais4joteamproject.domain.chat.dto.GetChatRoomResponse;
+import com.example.thetunais4joteamproject.domain.chat.dto.JoinChatRoomResponse;
 import com.example.thetunais4joteamproject.domain.chat.service.ChatRoomService;
 import com.example.thetunais4joteamproject.global.common.ApiResponse;
 import jakarta.validation.Valid;
@@ -9,6 +12,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,5 +34,34 @@ public class ChatRoomController {
         CreateChatRoomResponse createChatRoomResponse = chatRoomService.create(memberId, createChatRoomRequest);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.created(createChatRoomResponse));
+    }
+
+    @PostMapping("/{chatRoomId}/join")
+    public ResponseEntity<ApiResponse<JoinChatRoomResponse>> join(
+            @AuthenticationPrincipal Long memberId,
+            @PathVariable Long chatRoomId
+    ) {
+        JoinChatRoomResponse joinChatRoomResponse = chatRoomService.join(memberId, chatRoomId);
+
+        return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.ok(joinChatRoomResponse));
+    }
+
+    @GetMapping
+    public ResponseEntity<ApiResponse<GetChatRoomListResponse>> getAll(
+            @AuthenticationPrincipal Long memberId
+    ) {
+        GetChatRoomListResponse getChatRoomListResponse = chatRoomService.getAll(memberId);
+
+        return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.ok(getChatRoomListResponse));
+    }
+
+    @GetMapping("/{chatRoomId}")
+    public ResponseEntity<ApiResponse<GetChatRoomResponse>> getOne(
+            @AuthenticationPrincipal Long memberId,
+            @PathVariable Long chatRoomId
+    ) {
+        GetChatRoomResponse getChatRoomResponse = chatRoomService.getOne(memberId, chatRoomId);
+
+        return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.ok(getChatRoomResponse));
     }
 }
