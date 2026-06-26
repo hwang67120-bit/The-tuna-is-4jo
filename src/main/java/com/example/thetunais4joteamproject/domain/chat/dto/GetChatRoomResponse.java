04a728fccebad0115ba093647bf701fcfc36b823
hwang67120-bit <hwang67120@gmail.com>
2026-06-26 11:05:23 +0900

    feat: 채팅방리스트 리스폰스

---
+++ src/main/java/com/example/thetunais4joteamproject/domain/chat/dto/GetChatRoomResponse.java
@@ -0,0 +1,32 @@
+package com.example.thetunais4joteamproject.domain.chat.dto;
+
+import com.example.thetunais4joteamproject.domain.chat.entity.ChatRoom;
+import com.example.thetunais4joteamproject.domain.chat.entity.ChatRoomStatus;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record GetChatRoomResponse(
+        Long chatRoomId,
+        Long memberId,
+        Long adminId,
+        String title,
+        ChatRoomStatus status,
+        LocalDateTime createdAt,
+        List<GetChatMessageResponse> messages
+) {
+
+    public static GetChatRoomResponse of(
+            ChatRoom chatRoom,
+            List<GetChatMessageResponse> messages
+    ) {
+        return new GetChatRoomResponse(
+                chatRoom.getId(),
+                chatRoom.getMemberId(),
+                chatRoom.getAdminId(),
+                chatRoom.getTitle(),
+                chatRoom.getStatus(),
+                chatRoom.getCreatedAt(),
+                messages
+        );
+    }
+}
