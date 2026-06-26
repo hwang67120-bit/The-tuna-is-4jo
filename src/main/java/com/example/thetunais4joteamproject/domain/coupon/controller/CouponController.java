2b76742bfa506c99874bb5b5f50596553c4e9b01
sighingpotato <jot0202@naver.com>
2026-06-26 15:24:17 +0900

    feat: 사용자 쿠폰 조회 기능 추가

--- src/main/java/com/example/thetunais4joteamproject/domain/coupon/controller/CouponController.java
+++ src/main/java/com/example/thetunais4joteamproject/domain/coupon/controller/CouponController.java
@@ -1,7 +1,10 @@
 package com.example.thetunais4joteamproject.domain.coupon.controller;
 
+import java.util.List;
+
 import com.example.thetunais4joteamproject.domain.coupon.dto.IssueCouponRequest;
 import com.example.thetunais4joteamproject.domain.coupon.dto.IssueCouponResponse;
+import com.example.thetunais4joteamproject.domain.coupon.dto.MemberCouponInfoResponse;
 import com.example.thetunais4joteamproject.domain.coupon.service.CouponService;
 import com.example.thetunais4joteamproject.global.common.ApiResponse;
 import jakarta.validation.Valid;
@@ -9,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -37,5 +41,19 @@ public class CouponController {
 
         // ApiResponse를 적용하여 201 Created 반환
         return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.created(responseData));
+    }
+
+    /**
+     * 🔍 [GET] 사용자 보유 쿠폰 목록 조회
+     */
+    @GetMapping
+    public ResponseEntity<ApiResponse<List<MemberCouponInfoResponse>>> getMyCoupons(
+            @AuthenticationPrincipal
+            Long memberId
+    ) {
+        List<MemberCouponInfoResponse> responseData = couponService.getMyCoupons(memberId);
+
+        // ApiResponse.ok 적용, 보유 쿠폰이 0개여도 [] 빈 리스트 반환 방어
+        return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.ok(responseData));
     }
 }
--- src/main/java/com/example/thetunais4joteamproject/domain/coupon/repository/MemberCouponRepository.java
+++ src/main/java/com/example/thetunais4joteamproject/domain/coupon/repository/MemberCouponRepository.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 import com.example.thetunais4joteamproject.domain.coupon.entity.MemberCoupon;
 
 @Repository
-public interface MemberCouponRepository extends JpaRepository<MemberCoupon, Long> {
+public interface MemberCouponRepository extends JpaRepository<MemberCoupon, Long>, MemberCouponRepositoryCustom {
 
     // 중복 발급 방지: 특정 회원이 특정 쿠폰을 이미 보유하고 있는지 확인.
     boolean existsByMemberIdAndCouponId(Long memberId, Long couponId);
