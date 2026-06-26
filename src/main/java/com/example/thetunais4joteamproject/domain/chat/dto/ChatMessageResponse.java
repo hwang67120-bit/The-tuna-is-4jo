adc4c2d5b5767f556d7ecf86b3691c97312dc9b7
hwang67120-bit <hwang67120@gmail.com>
2026-06-26 10:57:49 +0900

    feat: 시간 생성 추가 채팅메세지추가

--- src/main/java/com/example/thetunais4joteamproject/domain/chat/dto/ChatMessageResponse.java
+++ src/main/java/com/example/thetunais4joteamproject/domain/chat/dto/ChatMessageResponse.java
@@ -1,20 +1,25 @@
 package com.example.thetunais4joteamproject.domain.chat.dto;
 
+import com.example.thetunais4joteamproject.domain.chat.entity.ChatMessage;
 import java.time.LocalDateTime;
 
 public record ChatMessageResponse(
+        Long messageId,
         Long chatRoomId,
         Long senderId,
         String content,
-        LocalDateTime sentAt
+        String messageType,
+        LocalDateTime createdAt
 ) {
 
-    public static ChatMessageResponse from(SendChatMessageRequest sendChatMessageRequest) {
+    public static ChatMessageResponse from(ChatMessage chatMessage) {
         return new ChatMessageResponse(
-                sendChatMessageRequest.chatRoomId(),
-                sendChatMessageRequest.senderId(),
-                sendChatMessageRequest.content(),
-                LocalDateTime.now()
+                chatMessage.getId(),
+                chatMessage.getChatRoomId(),
+                chatMessage.getSenderId(),
+                chatMessage.getContent(),
+                chatMessage.getMessageType(),
+                chatMessage.getCreatedAt()
         );
     }
 }
