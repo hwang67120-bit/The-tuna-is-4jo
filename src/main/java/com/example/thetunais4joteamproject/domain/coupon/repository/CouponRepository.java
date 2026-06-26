838eb31518379f7e4709b04819cc713245905ab8
sighingpotato <jot0202@naver.com>
2026-06-26 12:49:47 +0900

    feat: 쿠폰 생성 기능 추가

---
+++ src/main/java/com/example/thetunais4joteamproject/domain/coupon/repository/CouponRepository.java
@@ -0,0 +1,9 @@
+package com.example.thetunais4joteamproject.domain.coupon.repository;
+
+import com.example.thetunais4joteamproject.domain.coupon.entity.Coupon;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CouponRepository extends JpaRepository<Coupon, Long> {
+}
