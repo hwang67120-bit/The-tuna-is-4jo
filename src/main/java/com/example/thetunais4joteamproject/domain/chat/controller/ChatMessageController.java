2afaebafcfbf7b7733fd6508bbfad2e74c071123
hwang67120-bit <hwang67120@gmail.com>
2026-06-26 10:56:23 +0900

    feat: 메세지 컨트롤러

--- src/main/java/com/example/thetunais4joteamproject/domain/chat/controller/ChatMessageController.java
+++ src/main/java/com/example/thetunais4joteamproject/domain/chat/controller/ChatMessageController.java
@@ -2,6 +2,7 @@ package com.example.thetunais4joteamproject.domain.chat.controller;
 
 import com.example.thetunais4joteamproject.domain.chat.dto.ChatMessageResponse;
 import com.example.thetunais4joteamproject.domain.chat.dto.SendChatMessageRequest;
+import com.example.thetunais4joteamproject.domain.chat.service.ChatMessageService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
@@ -18,6 +19,7 @@ public class ChatMessageController {
     private static final Logger log = LoggerFactory.getLogger(ChatMessageController.class);
 
     private final SimpMessagingTemplate messagingTemplate;
+    private final ChatMessageService chatMessageService;
 
     @MessageMapping("/chat/message")
     public void sendMessage(
@@ -31,7 +33,7 @@ public class ChatMessageController {
                 sendChatMessageRequest.content()
         );
 
-        ChatMessageResponse chatMessageResponse = ChatMessageResponse.from(sendChatMessageRequest);
+        ChatMessageResponse chatMessageResponse = chatMessageService.create(sendChatMessageRequest);
 
         messagingTemplate.convertAndSend(
                 "/topic/chat/rooms/" + sendChatMessageRequest.chatRoomId(),
