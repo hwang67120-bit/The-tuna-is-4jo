338c4eeca33e6499087b8d7d3ffed49fd747954f
hwang67120-bit <hwang67120@gmail.com>
2026-06-26 19:21:04 +0900

    feat: 3분후 자동으로 채팅 종료 되는 서비스

--- src/main/java/com/example/thetunais4joteamproject/domain/chat/service/ChatRoomAutoCloseScheduler.java
+++ src/main/java/com/example/thetunais4joteamproject/domain/chat/service/ChatRoomAutoCloseScheduler.java
@@ -1,10 +1,11 @@
 package com.example.thetunais4joteamproject.domain.chat.service;
 
-import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
 
 @Slf4j
 @Component
