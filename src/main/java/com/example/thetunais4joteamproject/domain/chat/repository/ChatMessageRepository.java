a43d0392c11e726232c86ee89341e95872985e8a
hwang67120-bit <hwang67120@gmail.com>
2026-06-26 11:02:15 +0900

    feat: 채방 조회

--- src/main/java/com/example/thetunais4joteamproject/domain/chat/repository/ChatMessageRepository.java
+++ src/main/java/com/example/thetunais4joteamproject/domain/chat/repository/ChatMessageRepository.java
@@ -1,7 +1,10 @@
 package com.example.thetunais4joteamproject.domain.chat.repository;
 
 import com.example.thetunais4joteamproject.domain.chat.entity.ChatMessage;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
+
+    List<ChatMessage> findByChatRoomIdOrderByCreatedAtAsc(Long chatRoomId);
 }
