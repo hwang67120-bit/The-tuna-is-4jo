c92a918740fb1b21d85a28cf3e2139f3b9582882
devdong1231 <79241089+devdong1231@users.noreply.github.com>
2026-06-28 17:31:21 +0900

    feat: 환불 기능 관련하여 webhook 수정

--- src/main/java/com/example/thetunais4joteamproject/domain/infra/webhook/WebhookEventRepository.java
+++ src/main/java/com/example/thetunais4joteamproject/domain/infra/webhook/WebhookEventRepository.java
@@ -3,5 +3,4 @@ package com.example.thetunais4joteamproject.domain.infra.webhook;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface WebhookEventRepository extends JpaRepository<WebhookEvent, Long> {
-
 }
