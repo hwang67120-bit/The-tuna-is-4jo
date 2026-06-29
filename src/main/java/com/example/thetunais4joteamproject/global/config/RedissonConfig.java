75a35bb8d4624d226345fcd26394194325dcecfc
hwang67120-bit <hwang67120@gmail.com>
2026-06-29 16:45:47 +0900

    fix: RedissonConfig 수정

--- src/main/java/com/example/thetunais4joteamproject/global/config/RedissonConfig.java
+++ src/main/java/com/example/thetunais4joteamproject/global/config/RedissonConfig.java
@@ -3,6 +3,7 @@ package com.example.thetunais4joteamproject.global.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,12 +11,14 @@ import org.springframework.context.annotation.Configuration;
 public class RedissonConfig {
 
     @Bean
-    public RedissonClient redissonClient() {
+    public RedissonClient redissonClient(
+            @Value("${redisson.address}") String redissonAddress
+    ) {
         Config config = new Config();
-        // 로컬 환경의 Embedded Redis 또는 외부 Redis에 접근하기 위해 주소를 설정합니다.
+
         config.useSingleServer()
-              .setAddress("redis://127.0.0.1:6379");
-              
+                .setAddress(redissonAddress);
+
         return Redisson.create(config);
     }
 }
