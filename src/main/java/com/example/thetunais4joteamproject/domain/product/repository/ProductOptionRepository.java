6cfe1bd158920391b3c36fb728a8b1655820b1cd
sighingpotato <jot0202@naver.com>
2026-07-01 11:39:10 +0900

    fix: ProductOptionRepository import문 수정

--- src/main/java/com/example/thetunais4joteamproject/domain/product/repository/ProductOptionRepository.java
+++ src/main/java/com/example/thetunais4joteamproject/domain/product/repository/ProductOptionRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 
-import io.lettuce.core.dynamic.annotation.Param;
+import org.springframework.data.repository.query.Param;
 import jakarta.persistence.LockModeType;
 
 public interface ProductOptionRepository extends JpaRepository<ProductOption, Long> {
